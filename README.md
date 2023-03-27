849c0312c203a76635deb6d483bb791343941d07
Audrey Allen <audrey.allen@hse.ie>
2023-03-27 21:19:09 +0100

    Project Update March 27

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Pands-Project
-Pands Project
+# Pands-Project - Analysis on the Iris Dataset
+
+
+# This assignment required us to produce summary statistics for the whole dataset, as well as,
+# per category of a categorical variable. 
+# The dataset that I am going to analyse is the IRIS dataset.
+# I will start by describing the variables and descriptive statistics related to them,
+# and then I will present some insights about this dataset using graphs. My final steps in this analysis
+# will include correlation and regression analysis.
--- analysis.py
+++ analysis.py
@@ -1,8 +1,22 @@
+# References below
 # https://www.kaggle.com/code/sixteenpython/machine-learning-with-iris-dataset/notebook
+# https://www.geeksforgeeks.org/box-plot-and-histogram-exploration-on-iris-data/
+# https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+# https://rpubs.com/Karolina_G/848706
+
+# This assignment required us to produce summary statistics for the whole dataset, as well as,
+# per category of a categorical variable. 
+# The dataset that I am going to analyse is the IRIS dataset.
+# I will start by describing the variables and descriptive statistics related to them,
+# and then I will present some insights about this dataset using graphs. My final steps in this analysis
+# will include correlation and regression analysis.
+
+#Author: Audrey Allen
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Read in CSV file
 data = pd.read_csv('iris_csv.csv')
@@ -11,7 +25,9 @@ data.head()
 # Print the number of rows and columns in dataset
 print(data.shape)
 
-#Print summary of each variable using pandas describe function
+# Print summary of each variable using pandas describe function
+# This is an overall summary of all variables
+
 df = pd.DataFrame(data)
 data.describe()
 print(df.describe())
@@ -28,42 +44,63 @@ data.species.value_counts()
 
 #Output histogram of each variable to .png file 
 #sepallength
-x = (data["sepallength"])
-plt.hist(data["sepallength"])
+
+plt.figure(figsize = (10, 7))
+x = data["sepallength"]  
+plt.hist(x, bins = 20, color = "green")
 plt.title("Histogram of Variable Sepal Length - Iris Dataset")
-plt.xlabel("Sepal Length")
+plt.title("Sepal Length in cm")
+plt.xlabel("Sepal_Length_cm")
 plt.ylabel("Count")
-plt.legend()
-plt.hist(data["sepallength"], color='green')
 plt.savefig('sepallength.png') # Save to PNG file
 plt.show()
 
 #sepalwidth
-plt.hist(data["sepalwidth"])
+plt.figure(figsize = (10, 7))
+x = data["sepalwidth"]  
+plt.hist(x, bins = 20, color = "skyblue")
 plt.title("Histogram of Variable Sepal Width - Iris Dataset")
-plt.xlabel("Sepal Width")
+plt.title("Sepal Width in cm")
+plt.xlabel("Sepal_Width_cm")
 plt.ylabel("Count")
-plt.legend()
-plt.hist(data["sepalwidth"], color='skyblue')
 plt.savefig('sepalwidth.png') # Save to PNG file
 plt.show()
 
 #petallength
-plt.hist(data["petallength"])
+plt.figure(figsize = (10, 7))
+x = data["petallength"]  
+plt.hist(x, bins = 20, color = "yellow")
 plt.title("Histogram of Variable Petal Length - Iris Dataset")
-plt.xlabel("Petal Length")
+plt.title("Petal Length in cm")
+plt.xlabel("Petal_Length_cm")
 plt.ylabel("Count")
-plt.legend()
-plt.hist(data["petallength"], color='orange')
-plt.savefig('petallength.png') # Save to PNG file
+plt.savefig('Petallength.png') # Save to PNG file
 plt.show()
 
 #petalwidth
-plt.hist(data["petalwidth"])
+plt.figure(figsize = (10, 7))
+x = data["petalwidth"]  
+plt.hist(x, bins = 20, color = "brown")
 plt.title("Histogram of Variable Petal Width - Iris Dataset")
-plt.xlabel("Petal Width")
+plt.title("Petal Width in cm")
+plt.xlabel("Petal_Width_cm")
 plt.ylabel("Count")
-plt.legend()
-plt.hist(data["petalwidth"], color='brown')
-plt.savefig('petalwidth.png') # Save to PNG file
+plt.savefig('Petalwidth.png') # Save to PNG file
+plt.show()
+
+
+#boxpplot sepallength
+sns.boxplot( x=data["species"], y=data["sepallength"], palette="Blues") .set(title='BoxPlot Sepal Length');
+plt.show()
+
+#boxpplot sepalwidth
+sns.boxplot( x=data["species"], y=data["sepalwidth"], palette="pastel") .set(title='BoxPlot Sepal Width');
+plt.show()
+
+#boxpplot petallength
+sns.boxplot( x=data["species"], y=data["petallength"], palette="deep") .set(title='BoxPlot Petal Length');
+plt.show()
+
+#boxpplot petalwidth
+sns.boxplot( x=data["species"], y=data["petalwidth"], palette="muted") .set(title='BoxPlot Petal Width');
 plt.show()
